eb5614ec474bc0f35fdec8326e8da0c3b0668fdc
Nathan Gezahegne <nathandaniel2222@gmail.com>
2018-08-20 01:41:18 +0000

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,4 +3,14 @@
 
 def key_for_min_value(name_hash)
 
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
+    end
+  end
+  lowest_key
+
 end
